94a290dd8ca21c96e8d71614a1bca559e7ab1e7b
M Abrar Khalid <abrarprince471@gmail.com>
2022-09-12 18:08:07 +0500

    Information page done

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Login from "./screens/Login.jsx";
 import { Outlet, Route, Routes } from "react-router-dom";
 import Sidebar from "./screens/Sidebar.jsx";
 import Dashboard from "./screens/Dashboard.jsx";
+import Information from "./screens/information.jsx";
 
 function Main() {
   return (
     <div className="app_container">
       <Sidebar />
-      <Outlet />
+      <div className="main__dashboard_wrapper">
+        <Outlet />
+      </div>
     </div>
   );
 }
@@ -20,6 +23,12 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Main />}>
         <Route path="" element={<Dashboard />} />
+      </Route>
+      <Route path="/" element={<Main />}>
+        <Route path="/dashboard/information" element={<Information />} />
+      </Route>
+      <Route path="/" element={<Main />}>
+        <Route path="/dashboard/contact" element={<contact />} />
       </Route>
     </Routes>
   );
--- src/component/SidebarAction.jsx
+++ src/component/SidebarAction.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function SidebarAction({ to, title, svg, defaultChecked }) {
+function SidebarAction({ to, title, chart, defaultChecked }) {
   const navigate = useNavigate();
   return (
     <div className="nav__link">
@@ -11,6 +11,7 @@ function SidebarAction({ to, title, svg, defaultChecked }) {
         defaultChecked={defaultChecked}
         className="nav__link__input"
         name="nav__link__input"
+        chart="chart"
         onClick={() => navigate(to)}
       />
       <div className="nav__link__content"> {title} </div>
